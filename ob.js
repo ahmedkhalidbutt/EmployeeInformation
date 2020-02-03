145e399b98c87841540b8ead29d56f12abdf177f
AhmadButt00 <39210745+AhmadButt00@users.noreply.github.com>
2020-02-03 15:09:06 +0500

    Moved JSON to separate JS File

---
+++ ob.js
@@ -0,0 +1,209 @@
+var myData = {
+    "cities": [
+      {
+        "name": "Sialkot",
+        "softwareHouses": [
+          {
+            "name": "Kaarigar",
+            "location": "Kashmir Road",
+            "type": "Web Solutions",
+            "departments": [
+              {
+                "name": "Human Resource",
+                "employees": [
+                  {
+                    "name": "Ali ",
+                    "salary": "20000",
+                    "city": "Sialkot",
+                    "mobileNumber": "0111111111",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Muneeb",
+                    "salary": "15000",
+                    "city": "Sialkot",
+                    "mobileNumber": "0222222222",
+                    "type": "Part Time"
+                  }
+                ]
+              },
+              {
+                "name": "Development",
+                "employees": [
+                  {
+                    "name": "Subtain",
+                    "salary": "30000",
+                    "city": "Islamabad",
+                    "mobileNumber": "0333333333",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Ahmad",
+                    "salary": "30000",
+                    "city": "Sialkot",
+                    "mobileNumber": "0444444444",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Faseeh",
+                    "salary": "20000",
+                    "city": "Lahore",
+                    "mobileNumber": "0555555555",
+                    "type": "Part Time"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "name": "Experts Solution",
+            "location": "Abbot Road",
+            "type": "Mobile Solutions",
+            "departments": [
+              {
+                "name": "Human Resource",
+                "employees": [
+                  {
+                    "name": "Hassan ",
+                    "salary": "15000",
+                    "city": "Sialkot",
+                    "mobileNumber": "0666666666",
+                    "type": "Full Time"
+                  }
+                ]
+              },
+              {
+                "name": "Development",
+                "employees": [
+                  {
+                    "name": "Aleem",
+                    "salary": "20000",
+                    "city": "Sialkot",
+                    "mobileNumber": "0777777777",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Zarq",
+                    "salary": "10000",
+                    "city": "Wazirabad",
+                    "mobileNumber": "08888888888",
+                    "type": "Part Time"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Lahore",
+        "softwareHouses": [
+          {
+            "name": "HashLogics",
+            "location": "Johar Town",
+            "type": "Cross Platform Solutions",
+            "departments": [
+              {
+                "name": "Human Resource",
+                "employees": [
+                  {
+                    "name": "Hamza ",
+                    "salary": "20000",
+                    "city": "Lahore",
+                    "mobileNumber": "1111111111",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Tayyab",
+                    "salary": "15000",
+                    "city": "Lahore",
+                    "mobileNumber": "1222222222",
+                    "type": "Part Time"
+                  }
+                ]
+              },
+              {
+                "name": "Development",
+                "employees": [
+                  {
+                    "name": "Zayn",
+                    "salary": "35000",
+                    "city": "Rawalpindi",
+                    "mobileNumber": "1333333333",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Shanawar",
+                    "salary": "30000",
+                    "city": "Sialkot",
+                    "mobileNumber": "1444444444",
+                    "type": "Full Time"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "name": "NetSol Technologies",
+            "location": "Main Ghazi Road",
+            "type": "Web Solutions",
+            "departments": [
+              {
+                "name": "Human Resource",
+                "employees": [
+                  {
+                    "name": "Rizwan",
+                    "salary": "40000",
+                    "city": "Lahore",
+                    "mobileNumber": "1555555555",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Ihsan",
+                    "salary": "45000",
+                    "city": "Lahore",
+                    "mobileNumber": "1666666666",
+                    "type": "Full Time"
+                  }
+                ]
+              },
+              {
+                "name": "Development",
+                "employees": [
+                  {
+                    "name": "Saad",
+                    "salary": "50000",
+                    "city": "Lahore",
+                    "mobileNumber": "1777777777",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Danish",
+                    "salary": "40000",
+                    "city": "Islamabad",
+                    "mobileNumber": "18888888888",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Ajmal",
+                    "salary": "35000",
+                    "city": "Sialkot",
+                    "mobileNumber": "1999999999",
+                    "type": "Full Time"
+                  },
+                  {
+                    "name": "Qasim",
+                    "salary": "20000",
+                    "city": "Lahore",
+                    "mobileNumber": "1000000000",
+                    "type": "Part Time"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  
